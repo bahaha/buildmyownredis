32243cf613c2fa2678ac31a9d4d46d625bffe2b7
Cheng Che <clementcheng56@gmail.com>
2024-05-25 14:08:08 +0800

    refactor: context signal to support grateful shutdown

--- app/server.go
+++ app/server.go
@@ -1,26 +1,72 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
+	"os/signal"
+	"strconv"
+	"syscall"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+var (
+	logger = slog.New(slog.NewTextHandler(os.Stdout))
+)
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+func NewRedisServer(ctx context.Context, port int) {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt)
+	defer stop()
+
+	logger.Info("Starting TCP server", "port", port)
+
+	l, err := net.Listen("tcp", net.JoinHostPort("0.0.0.0", strconv.Itoa(port)))
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		logger.Error("Failed to bind to port", "error", err)
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	logger.Info("Listening for connections on port", "port", port)
+
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				select {
+				case <-ctx.Done():
+					// Server is shutting down, exit the goroutine
+					return
+				default:
+					logger.Error("Error accepting connection", "error", err)
+					continue
+				}
+			}
+			go handleConnection(ctx, conn)
+		}
+	}()
+
+	<-ctx.Done()
+	logger.Info("Shutting down server")
+}
+
+func handleConnection(ctx ctx.Context, conn net.Conn) {
+	defer conn.Close()
+
+	logger.Info("Accepted connection", "remote_addr", conn.RemoteAddr())
+
+	select {
+	case <-ctx.Done():
+		logger.Info("Connection closed due to server shutdown", "remote_addr", conn.RemoteAddr())
+		return
+	default:
+		// Handle the connection
 	}
+}
+
+func main() {
+	ctx := context.Background()
+	NewRedisServer(ctx, 6379)
 }
